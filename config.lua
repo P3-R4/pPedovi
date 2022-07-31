2042bf48efc7b3672117a586b24b79470b34f94b
P3-R4 <90536386+P3-R4@users.noreply.github.com>
2022-07-31 20:05:10 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -12,8 +12,8 @@ Config.Lista = {
 	{
 		spawnaj = true, -- ako je false onda se ped nece spawnati
 		pedic = `a_f_y_bevhills_01`, -- hash peda (https://docs.fivem.net/docs/game-references/ped-models/)
-		coords = vector4(430.0492, -800.113, 29.491, 98.0),  -- koordinate
-		gender = 'female', -- pol peda
+		koordinate = vector4(430.0492, -800.113, 29.491, 98.0),  -- koordinate
+		pol = 'female', -- pol peda
 		crtajblip = true , -- ako je false blip se nece crtati
 		imeblipa = 'Butik', -- naziv blipa
 		bojablipa = 49, -- boja blipa 
@@ -23,8 +23,8 @@ Config.Lista = {
 	{
 		spawnaj = true,
 		pedic = `cs_chengsr`,
-		coords = vector4(1856.3639, -1307.7751, 111.6046, 115.5788), 
-		gender = 'male',
+		koordinate = vector4(1856.3639, -1307.7751, 111.6046, 115.5788), 
+		pol = 'male',
 		crtajblip = false,
 		imeblipa = 'Prodaja kokaina',
 		bojablipa = 49,
@@ -34,8 +34,8 @@ Config.Lista = {
 	{
 		spawnaj = false,
 		pedic = `cs_chengsr`,
-		coords = vector4(1478.0773, -1348.6987, 96.9556, 115.5788), 
-		gender = 'male',
+		koordinate = vector4(1478.0773, -1348.6987, 96.9556, 115.5788), 
+		pol = 'male',
 		crtajblip = true,
 		imeblipa = 'Prodaja kokaina',
 		bojablipa = 49,
