8427eb5897800790a107113a5f36d409467b0831
P3-R4 <90536386+P3-R4@users.noreply.github.com>
2022-07-31 20:04:57 +0200

    Update pedovi.lua

--- pedovi.lua
+++ pedovi.lua
@@ -5,11 +5,11 @@ Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(500)
 		for k,v in pairs(Config.Lista) do
-			local playerCoords = GetEntityCoords(PlayerPedId())
-			local distance = #(playerCoords - v.coords.xyz)
+			local x = GetEntityCoords(PlayerPedId())
+			local distance = #(x - v.koordinate.xyz)
 			if distance < Config.Distanca and not pedovis[k] then
 				if v.spawnaj then 
-				local spawnajpeda = blizupeda(v.pedic, v.coords, v.gender, v.animDict, v.animName, v.scenario)
+				local spawnajpeda = blizupeda(v.pedic, v.koordinate, v.pol)
 				pedovis[k] = { spawnajpeda = spawnajpeda }
 				end
 			end
@@ -31,8 +31,8 @@ end)
 
 CreateThread(function()
 	for k,v in pairs(Config.Lista) do
-		if v.crtajblip then 
-		blip = AddBlipForCoord(v.coords.xyz)
+	if v.crtajblip then 
+		blip = AddBlipForCoord(v.koordinate.xyz)
 		SetBlipSprite(blip, v.blipsp)
 		SetBlipDisplay(blip, 4)
 		SetBlipScale(blip, 0.8)
@@ -41,41 +41,30 @@ CreateThread(function()
 		BeginTextCommandSetBlipName("STRING")
 		AddTextComponentString(v.imeblipa)
 		EndTextCommandSetBlipName(blip)
-		end
+	end
 	end
 end)
 
-function blizupeda(pedic, coords, gender, animDict, animName, scenario)
+function blizupeda(pedic, koordinate, pol)
 	RequestModel(pedic)
 	while not HasModelLoaded(pedic) do
-		Citizen.Wait(50)
+		Wait(50)
 	end
 	if Config.SmanjiZ then
-		spawnajpeda = CreatePed(Config.nebitno[gender], pedic, coords.x, coords.y, coords.z - 1.0, coords.w, false, true)
+		spawnajpeda = CreatePed(Config.nebitno[pol], pedic, koordinate.x, koordinate.y, koordinate.z - 1.0, koordinate.w, false, true)
 	else
-		spawnajpeda = CreatePed(Config.nebitno[gender], pedic, coords.x, coords.y, coords.z, coords.w, false, true)
+		spawnajpeda = CreatePed(Config.nebitno[pol], pedic, koordinate.x, koordinate.y, koordinate.z, koordinate.w, false, true)
 	end
 	SetEntityAlpha(spawnajpeda, 0, false)
 	FreezeEntityPosition(spawnajpeda, true)
 	SetEntityInvincible(spawnajpeda, true)
 	SetBlockingOfNonTemporaryEvents(spawnajpeda, true)
 
-	if animDict and animName then
-		RequestAnimDict(animDict)
-		while not HasAnimDictLoaded(animDict) do
-			Citizen.Wait(50)
-		end
-		TaskPlayAnim(spawnajpeda, animDict, animName, 8.0, 0, -1, 1, 0, 0, 0)
-	end
-	if scenario then
-		TaskStartScenarioInPlace(spawnajpeda, scenario, 0, true)
-	end
 	
-		for i = 0, 255, 51 do
-			Citizen.Wait(50)
-			SetEntityAlpha(spawnajpeda, i, false)
-		end
-
+	for i = 0, 255, 51 do
+	Wait(50)
+	SetEntityAlpha(spawnajpeda, i, false)
+	end
 	SetEntityAsMissionEntity(spawnajpeda, true, true)
 	return spawnajpeda	
 end
